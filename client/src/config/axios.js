cd3b01175e449a1652dd112c9254d2259defe2d7
Aashay Vanpal <arkhamknight.aashay@gmail.com>
2021-03-16 18:49:13 +0530

    Testing deployment v1.5

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,8 +1,8 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    // baseURL: 'https://aaswadcaterers.herokuapp.com'
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://aaswad.herokuapp.com'
+    // baseURL: 'http://localhost:3000'
     // baseURL: 'http://localhost:5000'
 
 })
