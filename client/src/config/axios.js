81560c72a6ee6ad1c5d89e58350555d1828873a5
Aashay Vanpal <arkhamknight.aashay@gmail.com>
2021-04-10 14:53:56 +0530

    customer view focus v1.0.3 fixed

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,8 +1,8 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    // baseURL: 'https://aaswadcaterers.herokuapp.com'
-    baseURL: 'http://localhost:5001'
+    baseURL: 'https://aaswadcaterers.herokuapp.com'
+    // baseURL: 'http://localhost:5001'
     // baseURL: 'http://localhost:5000'
 
 })
