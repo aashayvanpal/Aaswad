5a99c2025c22d9c1cd04eb882bcfd1c95d64b8b0
aashay vanpal <arkhamknight.aashay@gmail.com>
2021-12-27 09:25:27 +0530

    dashboard deploy test

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,52 +7,52 @@ import LoadingSpinner from './LoadingSpinner'
 
 
 const Dashboard = () => {
-    const [orders, setOrders] = useState([])
-    const [approves, setApproves] = useState([])
-    const [confirms, setConfirms] = useState([])
-    const [completed, setCompleted] = useState([])
+    // const [orders, setOrders] = useState([])
+    // const [approves, setApproves] = useState([])
+    // const [confirms, setConfirms] = useState([])
+    // const [completed, setCompleted] = useState([])
 
     // const [items, setItems] = useState([])
     // const [itemsDisplay, setItemsDisplay] = useState([])
     const [loading, setLoading] = useState(true)
 
 
-    useEffect(() => {
-        console.log('inside use effect')
+    // useEffect(() => {
+    //     console.log('inside use effect')
 
-        setTimeout(() => setLoading(true), 3000)
+    //     setTimeout(() => setLoading(true), 3000)
 
-        axios.get('/api/orders', {
-            headers: {
-                'x-auth': localStorage.getItem('token')
-            }
-        })
-            .then(response => {
-                // console.log('Data : ', response.data)
-                const orders = response.data
-                console.log('orders after request :', orders)
-                setOrders(orders)
-                // filter for approve 
-                const approves = orders.filter(order => order.status === 'approve')
-                console.log('approves filtered:', approves)
-                setApproves(approves)
-                // filter for confirmed 
-                const confirms = orders.filter(order => order.status === 'confirmed')
-                console.log('confirms filtered:', confirms)
-                setConfirms(confirms)
+    //     axios.get('/api/orders', {
+    //         headers: {
+    //             'x-auth': localStorage.getItem('token')
+    //         }
+    //     })
+    //         .then(response => {
+    //             // console.log('Data : ', response.data)
+    //             const orders = response.data
+    //             console.log('orders after request :', orders)
+    //             setOrders(orders)
+    //             // filter for approve 
+    //             const approves = orders.filter(order => order.status === 'approve')
+    //             console.log('approves filtered:', approves)
+    //             setApproves(approves)
+    //             // filter for confirmed 
+    //             const confirms = orders.filter(order => order.status === 'confirmed')
+    //             console.log('confirms filtered:', confirms)
+    //             setConfirms(confirms)
 
 
-                // filter for completed 
-                const completed = orders.filter(order => order.status === 'completed')
-                console.log('completed filtered:', completed)
-                setCompleted(completed)
+    //             // filter for completed 
+    //             const completed = orders.filter(order => order.status === 'completed')
+    //             console.log('completed filtered:', completed)
+    //             setCompleted(completed)
 
-                setLoading(false)
-            })
+    //             setLoading(false)
+    //         })
 
-            .catch(err => {
-                console.log(err)
-            })
+    //         .catch(err => {
+    //             console.log(err)
+    //         })
 
         // axios.get('/api/items', {
         //     headers: {
@@ -71,7 +71,7 @@ const Dashboard = () => {
         //     .catch(err => {
         //         console.log(err)
         //     })
-    }, [1])
+    // }, [1])
 
     return (
         <div>
@@ -81,7 +81,7 @@ const Dashboard = () => {
             <ShowBtn />
             {loading ? (<div>
                 <h1>Orders</h1>
-                <div style={{ display: "inline-flex" }}>
+                {/* <div style={{ display: "inline-flex" }}>
                     <div style={{ border: "2px solid black", borderRadius: "5px", margin: "10px", textAlign: "center" }}>
                         <h2>Total orders : {orders.length}</h2>
                     </div>
@@ -94,7 +94,7 @@ const Dashboard = () => {
                     <div style={{ border: "2px solid black", borderRadius: "5px", margin: "10px", textAlign: "center" }}>
                         <h2>Completed orders :{completed.length}</h2>
                     </div>
-                </div>
+                </div> */}
                 {/* <h1>Items</h1>
                 <div style={{ display: "inline-flex" }}>
                     <div style={{ border: "2px solid black", borderRadius: "5px", margin: "10px", textAlign: "center" }}>
