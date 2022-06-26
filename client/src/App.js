48ca259e5d70c57e05edf6799dc726067e133885
Aashay <arkhamknight.aashay@gmail.com>
2022-06-26 11:02:49 +0530

    v1.0.6.0 T1 Complete refactored code

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-// App version v1.0.5.15 T1 refactored Item form file  
+// App version v1.0.6.0 T1 Complete Refactored Code
 // Check version-notes.txt for version updates
 // Working on css for both desktop and mobile(only customer view)
 import React from "react";
